ab3ee6b73eb8e98b2de9a2bb2cbd0088676893b0
Ashwin Bansod <ashwinbansod@yahoo.co.in>
2016-07-29 16:09:44 -0500

    Updated Copyright policy and added links for NSF, UIC and CompBio
    Updated Copyright policy and added links for NSF, UIC and CompBio

--- Front_end/html/inc/footer.php
+++ Front_end/html/inc/footer.php
@@ -9,17 +9,17 @@
             <div class="secondLine">
                 <div class="pageWidth positioning">
                     <div class="footBirds"></div>
-                    <a href="#" class="nsf">
+                    <a href="https://www.nsf.gov" class="nsf">
                     </a>
-                    <a href="#" class="uic">
+                    <a href="https://www.uic.edu" class="uic">
                     </a>
-                    <a href="#" class="oth">
+                    <a href="http://compbio.cs.uic.edu" class="oth">
                     </a>
                 </div>
             </div>
             <div class="thirdLine">
                 <div class="pageWidth">
-                    COPYRIGHT &copy; 2012 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF ILLINOIS
+                    COPYRIGHT &copy; 2012 2016 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF ILLINOIS
                 </div>
             </div>
         </div>
