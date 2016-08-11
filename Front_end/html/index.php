38cf2290d6f8075f11910879c964c2de473fd3e6
Ashwin Bansod <ashwinbansod@yahoo.co.in>
2016-08-11 17:40:07 -0500

    Moved index2.php to index.php to make the website live and available to public.
    Copied the out of service index.php to index_out_of_service.php and
    copied index2.php to index.php which made the website live and
    accessible to all public.

--- Front_end/html/index.php
+++ Front_end/html/index.php
@@ -12,24 +12,62 @@
 <?php include("inc/header.php"); ?>
 
 
-<p><p>
-<center>
-<font size="5" color="black"><b>
-<p>&nbsp;<p><p>&nbsp;<p><p>&nbsp;<p><p>&nbsp;<p>
-Kinalyzer is temporarily unavailable while we make improvements to our service.<p>&nbsp;<p>
-Apologies for the inconvenience. Please check back shortly.
-<p>&nbsp;<p>
-If you have any questions please email us at <a href="mailto:kinalyzer@cs.uic.edu">kinalyzer@cs.uic.edu</a>.
-<p>&nbsp;<p><p>&nbsp;<p><p>&nbsp;<p><p>&nbsp;<p>
-</b></font>
-</center>
-<p><p>
-
+                    <div class="signUp positioning">
+                        <div class="stepTooltip step1">
+                            <div class="positioning">
+                                <div class="toolArrow"></div>
+                                <span>Step 1: Enter Your Name</span>
+                                <p>We will only use this information to Kinalyze your data. We will not contact you with offers or give it away.</p>
+                            </div>
+                        </div>
+                        <div class="stepTooltip step2">
+                            <div class="positioning">
+                                <div class="toolArrow"></div>
+                                <span>Step 2: Enter Your E-Mail</span>
+                                <p>We will only use this information to Kinalyze your data. We will not contact you with offers or give it away.</p>
+                            </div>
+                        </div>
+                        <div class="stepTooltip step3">
+                            <div class="positioning">
+                                <div class="toolArrow"></div>
+                                <span>Step 3: Select Algorithm</span>
+                                <p>We will only use this information to Kinalyze your data. We will not contact you with offers or give it away.</p>
+                            </div>
+                        </div>
+                        <img src="images/duck.jpg" alt="Duck" />
+                        <img src="images/fish.jpg" alt="Fishes" />
+                        <img src="images/bear.jpg" alt="Bear" />
+                        <form action="data.php" method="post" enctype="multipart/form-data" id="kin_form">
+                            <div class="label nameInput"></div><input type="text" name="user" class="input_name" placeholder="Name"><br>
+                            <div class="label emailInput"></div><input type="email" name="email" class="input_email" placeholder="Email"><br>
+                            <div class="label algInput"></div><div class="algDrop"></div>
+                            <select id="source" name="algo">
+                                <option value="-1" selected="selected">Choose an Algorithm</option>
+                                <option value="0">2-Allele</option>
+                                <option value="1">Consensus</option>
+                            </select>
+                            <br><br>
+                            <span class="file-input-wrapper">
+                                <a href="#" class="choose button btn-file-input">Choose File</a>
+                                <input type="file" name="input_file" id="fileToUpload">
+                            </span>
+                            <a href="#" class="continue button grayed">Continue</a>
+                            <input TYPE="hidden" name="Kinalyze" value="Kinalize">
+                        </form>
+                    </div>
+                </div>
+                <div class="quote">
+                    <div class="pageWidth">
+                        <blockquote><span></span>"I do not believe that the accident of birth makes people sisters and brothers.<br>
+                        It makes them siblings. Gives them mutuality of parentage."</blockquote>
+                        <cite>--Maya Angelou</cite>
+                    </div>
+                </div>
                 <div class="information">
                     <div class="pageWidth">
                         <div class="aboutUs">
                             <h2>About the Kinalyzer</h2>
-                            <p>Kinalyzer is a sofware suite developed to reconstruct sibling groups using genotypes from codominant markers such as microsatellites. Currently there are two algorithms available to reconstruct full-sibling groups for cases where parental genotypes are not available. Kinalyzer uses combinatorial optimization based on Mendelian inheritance rules to find the fewest number of sibling groups that contain all the individuals in the sample ('2-allele set cover'). Also available is a 'concensus' method that reconstructs sibgroups using subsets of loci and finds the consensus of these different solutions. A sample dataset can be found <a href="sample-data/samplefile1.txt">here</a>.</p>
+                            <p>Kinalyzer is a sofware suite developed to reconstruct sibling groups using genotypes from codominant markers such as microsatellites. Currently there are two algorithms available to reconstruct full-sibling groups for cases where parental genotypes are not available. Kinalyzer uses combinatorial optimization based on Mendelian inheritance rules to find the fewest number of sibling groups that contain all the individuals in the sample '2-allele set cover'. Also available is a 'Consensus' method that reconstructs sibgroups using subsets of loci and finds the consensus of these different solutions. A sample dataset can be found <a href="sample-data/samplefile1.txt">here</a>.</p>
                         </div>
                         <br class="clear">
                     </div>
